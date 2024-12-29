76e8f297e7699ac66f90fd249d58e83f1e6b85f4
cristopher joshua olvera alvarez <cristopher_170498@hotmail.com>
2024-12-29 14:47:15 -0600

    Cambios en recursos

--- msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -13,6 +13,7 @@ import org.aguzman.springcloud.msvc.usuarios.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -35,14 +36,22 @@ public class UsuarioController {
     @Autowired
     private ApplicationContext context;
 
+    @Autowired
+    private Environment env;    
+
     @GetMapping("/crash")
     public void crash() {
         ((ConfigurableApplicationContext) context).close();
     }
 
     @GetMapping
-    public Map<String, List<Usuario>> listar() {
-        return Collections.singletonMap("usuarios", service.listar());
+    public Map<String, Object> listar() {
+        Map<String,Object> body = new HashMap<>();
+        body.put("users",service.listar());
+        body.put("podinfo", env.getProperty("MY_POD_NAME")+ " "+ env.getProperty("MY_POD_IP"));
+        body.put("texto",env.getProperty("config.texto"));
+        // return Collections.singletonMap("usuarios", service.listar());
+        return body;
     }   
 
     @GetMapping("/{id}")
